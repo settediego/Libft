dc3ecb69e93dd9adbfb42b068ad99c1ed8dd8cf4
Diego Moreira <diegomor@f0r1s4.codam.nl>
2024-10-13 22:05:33 +0200

    my first day working with the libft project

---
+++ ft_memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegomor <diegomor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 17:39:02 by diegomor          #+#    #+#             */
+/*   Updated: 2024/10/13 19:28:27 by diegomor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void *str, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)str;
+	while (n > 0)
+	{
+		*ptr = c;
+		ptr++;
+		n--;
+	}
+	return (str);
+}
