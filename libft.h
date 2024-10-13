fe763bcb15d160dc5859edd02b91b990035fd453
Diego Moreira <diegomor@f0r1s4.codam.nl>
2024-10-13 22:33:43 +0200

    creates first version of libft.h

---
+++ libft.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegomor <diegomor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 22:32:19 by diegomor          #+#    #+#             */
+/*   Updated: 2024/10/13 22:32:51 by diegomor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+// Standard Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+#include <errno.h>
+#include <limits.h>
+#include <float.h>
+#include <unistd.h>
+
+// Character Functions
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+
+// String Functions
+int		ft_strlen(char *str);
+
+// Memory Functions
+void	ft_bzero(void *str, size_t n);
+void	*ft_memset(void *str, int c, size_t n);
+
+
+
+
+#endif
