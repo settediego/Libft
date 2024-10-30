e43cb496726968b44478b04965ec9202d29f6aa9
Diego Moreira <diegomor@f0r3s8.codam.nl>
2024-10-30 17:48:03 +0100

    MODIFY ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,21 +6,21 @@
 /*   By: diegomor <diegomor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 19:01:18 by diegomor          #+#    #+#             */
-/*   Updated: 2024/10/30 17:18:13 by diegomor         ###   ########.fr       */
+/*   Updated: 2024/10/30 17:47:40 by diegomor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t num, size_t size)
 {
 	unsigned char	*dest;
 
-	if (nmemb && size > INT_MAX / nmemb)
+	if (num && size * num > INT_MAX)
 		return (NULL);
-	dest = (unsigned char *)malloc(nmemb * size);
+	dest = (unsigned char *)malloc(size * num);
 	if (!dest)
 		return ((void *)0);
-	ft_bzero(dest, nmemb * size);
+	ft_bzero(dest, num * size);
 	return ((void *)dest);
 }
